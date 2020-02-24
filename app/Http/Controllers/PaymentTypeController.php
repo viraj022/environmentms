7259e8ac8ac74e97bf8842ce7f6a669bb981ff7f
hansana <hansanacham@gmail.com>
2020-02-24 12:58:23 +0530

    Payment Type view model and migration

---
+++ app/Http/Controllers/PaymentTypeController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PaymentType;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PaymentTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        $pageAuth = $user->authentication(config('auth.privileges.industry'));
+        return view('payment_type', ['pageAuth' => $pageAuth]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PaymentType  $paymentType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PaymentType $paymentType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PaymentType  $paymentType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PaymentType $paymentType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PaymentType  $paymentType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PaymentType $paymentType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PaymentType  $paymentType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PaymentType $paymentType)
+    {
+        //
+    }
+}
---
+++ resources/views/payment_type.blade.php
@@ -0,0 +1,272 @@
+@extends('layouts.admin')
+@extends('layouts.styles')
+@extends('layouts.scripts')
+@extends('layouts.navbar')
+@extends('layouts.sidebar')
+@extends('layouts.footer')
+@section('pageStyles')
+<!-- Select2 -->
+<link rel="stylesheet" href="/plugins/select2/css/select2.min.css">
+<link rel="stylesheet" href="/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+<!-- Bootstrap4 Duallistbox -->
+<link rel="stylesheet" href="/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="/dist/css/adminlte.min.css">
+<!-- Google Font: Source Sans Pro -->
+@endsection
+@section('content')
+@if($pageAuth['is_read']==1 || false)
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-12 col-sm-6">
+                <h1>Pradesheyasaba</h1>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-5">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <label id="lblTitle">Add New Pradesiyasaba</label>
+                    </div>
+                    <div class="card-body">
+                        <label>Name*</label>
+                        <input id="getName" type="text" class="form-control form-control-sm"
+                               placeholder="Enter Name..."
+                               value="">
+                        <div id="valName" class="d-none"><p class="text-danger">Name is required</p></div>
+                        <div id="valUnique" class="d-none"><p class="text-danger">Name already taken!</p></div>
+                    </div>
+                    <div class="card-body">
+                        <label>Code*</label>
+                        <input id="getCode" type="text" class="form-control form-control-sm"
+                               placeholder="Enter Code..."
+                               value="">
+                        <div id="valCode" class="d-none"><p class="text-danger">Code is required</p></div>
+                    </div>
+
+                    <div class="card-footer">
+                        @if($pageAuth['is_create']==1 || false)
+                        <button id="btnSave" type="submit" class="btn btn-primary">Save</button>
+                        @endif
+                        @if($pageAuth['is_update']==1 || false)
+                        <button id="btnUpdate" type="submit" class="btn btn-warning d-none">Update</button>
+                        @endif
+                        @if($pageAuth['is_delete']==1 || false)
+                        <button  id="btnshowDelete" type="submit" class="btn btn-danger d-none"  data-toggle="modal"
+                                 data-target="#modal-danger">Delete</button>
+                        @endif
+                    </div>                           
+                </div>
+            </div>
+
+
+            <div class="col-md-7">
+                <div class="card card-primary">
+                    <div class="card-body">
+                        <div class="row">
+
+                            <div class="col-md-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h3 class="card-title">Pradesiyasaba</h3>
+                                    </div>
+                                    <!-- /.card-header -->
+                                    <div class="card-body p-0">
+                                        <table class="table table-condensed assignedPrivilages" id="tblPradesiyasaba">
+                                            <thead>
+                                                <tr>
+                                                    <th style="width: 10px">#</th>
+                                                    <th>Name</th>
+                                                    <th>Code</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <!-- /.card-body -->
+                                </div>
+                            </div>                                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<div class="modal fade" id="modal-danger">
+    <div class="modal-dialog">
+        <div class="modal-content bg-danger">
+            <div class="modal-header">
+                <h4 class="modal-title">Delete  Pradesiyasaba</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p><b>Are you sure you want to permanently delete this  Pradesiyasaba ? </b></p>
+                <p>Once you continue, this process can not be undone. Please Procede with care.</p>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-outline-light" data-dismiss="modal">Close</button>
+                <button id="btnDelete" type="submit" class="btn btn-outline-light" data-dismiss="modal">Delete Permanently</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+</section>
+@endif
+@endsection
+
+
+
+@section('pageScripts')
+<!-- Page script -->
+
+<!-- Select2 -->
+<script src="../../plugins/select2/js/select2.full.min.js"></script>
+<!-- Bootstrap4 Duallistbox -->
+<script src="../../plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+<!-- InputMask -->
+<script src="../../plugins/moment/moment.min.js"></script>
+<script src="../../plugins/inputmask/min/jquery.inputmask.bundle.min.js"></script>
+<!-- date-range-picker -->
+<script src="../../plugins/daterangepicker/daterangepicker.js"></script>
+<!-- bootstrap color picker -->
+<script src="../../plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="../../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Bootstrap Switch -->
+<script src="../../plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+<script src="../../js/pradeshiyasabajs/submit.js"></script>
+<script src="../../js/pradeshiyasabajs/get.js"></script>
+<script src="../../js/pradeshiyasabajs/update.js"></script>
+<script src="../../js/pradeshiyasabajs/delete.js"></script>
+<!-- AdminLTE App -->
+<script>
+    $(function () {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 4000
+
+        });
+//Load table
+        loadTable();
+//click save button
+        $('#btnSave').click(function () {
+            var data = fromValues();
+            if (Validiteinsert(data)) {
+                // if validiated
+                AddPradeshiyasaba(data, function (result) {
+                    if (result.id == 1) {
+                        Toast.fire({
+                            type: 'success',
+                            title: 'Enviremontal MS</br>Saved'
+                        });
+                    } else {
+                        Toast.fire({
+                            type: 'error',
+                            title: 'Enviremontal MS</br>Error'
+                        });
+                    }
+                    loadTable();
+                    resetinputFields();
+                });
+            }
+        });
+//click update button
+        $('#btnUpdate').click(function () {
+            //get form data
+            var data = fromValues();
+            if (Validiteupdate(data)) {
+                updatePradesheeyasaba($('#btnUpdate').val(), data, function (result) {
+                    if (result.id == 1) {
+                        Toast.fire({
+                            type: 'success',
+                            title: 'Enviremontal MS</br>Updated'
+                        });
+                    } else {
+                        Toast.fire({
+                            type: 'error',
+                            title: 'Enviremontal MS</br>Error'
+                        });
+                    }
+                    loadTable();
+                    showSave();
+                    resetinputFields();
+                });
+            }
+        });
+//click delete button
+        $('#btnDelete').click(function () {
+            deletePradesheeyasaba($('#btnDelete').val(), function (result) {
+                if (result.id == 1) {
+                    Toast.fire({
+                        type: 'success',
+                        title: 'Enviremontal MS</br>Removed!'
+                    });
+                } else {
+                    Toast.fire({
+                        type: 'error',
+                        title: 'Enviremontal MS</br>Error'
+                    });
+                }
+                loadTable();
+                showSave();
+                resetinputFields();
+            });
+        });
+//select button action 
+        $(document).on('click', '.btnAction', function () {
+            getaPradesiyasababyId(this.id, function (result) {
+                $('#getName').val(result.name);
+                $('#getCode').val(result.code);
+                showUpdate();
+                $('#btnUpdate').val(result.id);
+                $('#btnDelete').val(result.id);
+            });
+        });
+    });
+//show update buttons    
+    function showUpdate() {
+        $('#btnSave').addClass('d-none');
+        $('#btnUpdate').removeClass('d-none');
+        $('#btnshowDelete').removeClass('d-none');
+    }
+//show save button    
+    function showSave() {
+        $('#btnSave').removeClass('d-none');
+        $('#btnUpdate').addClass('d-none');
+        $('#btnshowDelete').addClass('d-none');
+    }
+//Reset all fields    
+    function resetinputFields() {
+        $('#getName').val('');
+        $('#getCode').val('');
+        $('#btnUpdate').val('');
+        $('#btnDelete').val('');
+    }
+//get form values
+    function fromValues() {
+        var data = {
+            name: $('#getName').val(),
+            code: $('#getCode').val()
+        };
+        return data;
+    }
+</script>
+@endsection
