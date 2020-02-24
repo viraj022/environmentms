4077f6a6036282f6b62a91164415775ef41ebc4e
hansana <hansanacham@gmail.com>
2020-02-24 16:08:37 +0530

    add payments table

--- app/PaymentType.php
+++ app/PaymentType.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use Faker\Provider\ar_SA\Payment;
 use Illuminate\Database\Eloquent\Model;
 
 class PaymentType extends Model
 {
-    //
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
 }
