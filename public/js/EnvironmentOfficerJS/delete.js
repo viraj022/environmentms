feb589d494229792c17953775dc9df8d5b8f11c0
Nadun Nilanga <nadun@ceytechsystemsolutions.com>
2020-03-23 18:31:00 +0530

    env officer

---
+++ public/js/EnvironmentOfficerJS/delete.js
@@ -0,0 +1,24 @@
+function deleteEnviremontalOfficer(id,callBack){
+    //just change active status = 0
+    $.ajax({
+        type: "delete",
+        headers: {
+            "Authorization": "Bearer " + $('meta[name=api-token]').attr("content"),
+            "Accept": "application/json"
+        },
+        url: "api/environment_officers/id/"+id,
+        data: null,
+        dataType: "json",
+        cache: false,
+        processDaate: false,
+        success: function (result) {
+
+            if (typeof callBack !== 'undefined' && callBack != null && typeof callBack === "function") {
+        
+                callBack(result);
+            }
+        }
+    });
+
+    
+}
---
+++ public/js/EnvironmentOfficerJS/update.js
@@ -0,0 +1,36 @@
+function updateAssistantDir(id, data, callBack) {
+    $.ajax({
+        type: "PUT",
+        headers: {
+            "Authorization": "Bearer " + $('meta[name=api-token]').attr("content"),
+            "Accept": "application/json"
+        },
+        url: "api/zone/id/" + id,
+        data: data,
+        dataType: "json",
+        cache: false,
+        processDaate: false,
+        success: function (result) {
+
+            if (typeof callBack !== 'undefined' && callBack != null && typeof callBack === "function") {
+                callBack(result);
+            }
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            alert(textStatus + ':' + errorThrown);
+        }
+    });
+}
+
+function Validiteupdate(data) {
+    var response = true;
+    if (data.name.length == 0) {
+        $('#valName').removeClass('d-none');
+        response = false;
+    }
+    if (data.code.length == 0) {
+        $('#valCode').removeClass('d-none');
+        response = false;
+    }
+    return response;
+}
