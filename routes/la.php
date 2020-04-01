b4b6268525a0ece08efee8e61bc29aecad024b84
hansana <hansanacham@gmail.com>
2020-04-01 12:43:01 +0530

    dsa

--- routes/la.php
+++ routes/la.php
@@ -4,3 +4,4 @@ Route::post('/localAuthority','LocalAuthorityController@create');
 Route::get('/localAuthority/id/{id}','LocalAuthorityController@edit');
 Route::put('/localAuthority/id/{id}','LocalAuthorityController@store');
 Route::delete('/localAuthority/id/{id}','LocalAuthorityController@delete');
+//
