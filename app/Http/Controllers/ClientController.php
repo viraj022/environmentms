7d720ffd4f8093043f23782b3080c69674e8ef73
Nadun Nilanga <nadun@ceytechsystemsolutions.com>
2020-04-01 18:36:31 +0530

    client backend

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ClientController extends Controller
 {
@@ -24,8 +25,42 @@ class ClientController extends Controller
      */
     public function create()
     {
-        //
-    }
+
+     $user = Auth::user();
+     $pageAuth = $user->authentication(config('auth.privileges.clientSpace'));
+     request()->validate([
+         'first_name' => 'required',
+         'last_name' => 'required',
+         // 'address' => 'required',
+         // 'contact_no' => 'required',
+         // 'email' => 'required',
+         'nic' => 'required',
+         // 'password' => 'required',
+     ]);
+     if ($pageAuth['is_create']) {
+         $client = new Client(); 
+         $client->first_name = \request('first_name'); 
+         $client->last_name = \request('last_name'); 
+         $client->address = \request('address'); 
+         $client->contact_no = \request('contact_no'); 
+         $client->email = \request('email'); 
+         $client->nic = \request('nic'); 
+         $client->password = \request('password'); 
+         $msg = $client->save(); 
+         if ($msg) {
+             return array('id' => 1, 'message' => 'true'); 
+         } 
+         else 
+         { 
+             return array('id' => 0, 'message' => 'false'); 
+         } 
+     } 
+     else 
+     { 
+      abort(401); 
+  }  
+
+}
 
     /**
      * Store a newly created resource in storage.
@@ -33,10 +68,44 @@ class ClientController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store($id)
     {
-        //
-    }
+
+       $user = Auth::user();
+       $pageAuth = $user->authentication(config('auth.privileges.clientSpace'));
+       request()->validate([
+           'first_name' => 'required',
+           'last_name' => 'required',
+           'address' => 'required',
+           'contact_no' => 'required',
+           'email' => 'required',
+           'nic' => 'required',
+           'password' => 'required',
+       ]);
+       if ($pageAuth['is_update']) {
+           $client = Client::findOrFail($id); 
+           $client->first_name= \request('first_name'); 
+           $client->last_name= \request('last_name'); 
+           $client->address= \request('address'); 
+           $client->contact_no= \request('contact_no'); 
+           $client->email= \request('email'); 
+           $client->nic= \request('nic'); 
+           $client->password= \request('password'); 
+           $msg = $client->save(); 
+           if ($msg) {
+               return array('id' => 1, 'message' => 'true'); 
+           }  
+           else 
+           { 
+               return array('id' => 0, 'message' => 'false'); 
+           } 
+       } 
+       else 
+       { 
+          abort(401); 
+      }  
+
+  }
 
     /**
      * Display the specified resource.
@@ -44,10 +113,16 @@ class ClientController extends Controller
      * @param  \App\Client  $client
      * @return \Illuminate\Http\Response
      */
-    public function show(Client $client)
+    public function show()
     {
-        //
-    }
+        $user = Auth::user(); 
+        $pageAuth = $user->authentication(config('auth.privileges.clientSpace')); 
+        if ($pageAuth['is_read']) { 
+          return Client::get(); 
+      } else 
+      { 
+        abort(401); 
+    }     }
 
     /**
      * Show the form for editing the specified resource.
@@ -78,8 +153,40 @@ class ClientController extends Controller
      * @param  \App\Client  $client
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Client $client)
+    public function destroy($id)
     {
-        //
+
+       $user = Auth::user();  
+       $pageAuth = $user->authentication(config('auth.privileges.clientSpace'));   
+       if ($pageAuth['is_delete']) {  
+           $client = Client::findOrFail($id); 
+           $msg =  $client->delete();  
+           if ($msg) {  
+               return array('id' => 1, 'message' => 'true');  
+           } else {   
+               return array('id' => 0, 'message' => 'false');  
+           } 
+       } else { 
+           abort(401); 
+       }
+   }
+
+
+
+
+    public function findClient_by_nic($nic)
+    {
+          $user = Auth::user();
+    $pageAuth = $user->authentication(config('auth.privileges.clientSpace'));
+    if ($pageAuth['is_read']) {
+     //    PaymentType::get();
+           return Client::where('nic','=',$nic)
+            ->get();
+
+                } else {
+        abort(401);
     }
+    }
+
+
 }
--- config/auth.php
+++ config/auth.php
@@ -124,7 +124,7 @@ return [
         'zone' => 7,
         'assistantDirector' => 8,
         'environmentOfficer' => 9,
-        'client_space' => 10,        
+        'clientSpace' => 10,        
     ],
 
 ];
