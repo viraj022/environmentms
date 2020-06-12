e1f824ea7b5ca154cc7d9bda962e41f558d0a938
Jake Logan <castleofae@gmail.com>
2020-06-12 12:20:26 +0530

    updates

--- public/js/CommitteeJS/submit.js
+++ public/js/CommitteeJS/submit.js
@@ -35,7 +35,9 @@ function Validiteinsert(data) {
                 if (data.nic.length == 10) {
                     var str = data.nic;
                     var res = str.slice(0, 9);
-                    if (!isNaN(res)) {
+                   
+                    if (isNaN(res)) {
+                         alert(res);
                         $('#valNic').removeClass('d-none');
                         return false;
                     }
