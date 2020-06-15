f167f02529844c1f92ac9083b9e81141accc914b
Jake Logan <castleofae@gmail.com>
2020-06-15 16:24:33 +0530

    msg

--- app/Http/Controllers/EPLController.php
+++ app/Http/Controllers/EPLController.php
@@ -12,19 +12,18 @@ use App\Payment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class EPLController extends Controller
-{
-    public function __construct()
-    {
+class EPLController extends Controller {
+
+    public function __construct() {
         $this->middleware(['auth']);
     }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($id)
-    {
+    public function index($id) {
         $user = Auth::user();
         $pageAuth = $user->authentication(config('auth.privileges.EnvironmentProtectionLicense'));
         if ($pageAuth['is_read']) {
@@ -38,8 +37,7 @@ class EPLController extends Controller
         }
     }
 
-    public function profile($client, $profile)
-    {
+    public function profile($client, $profile) {
         $user = Auth::user();
         $pageAuth = $user->authentication(config('auth.privileges.EnvironmentProtectionLicense'));
         if ($pageAuth['is_read']) {
@@ -58,86 +56,85 @@ class EPLController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
+    public function create() {
         $user = Auth::user();
         $pageAuth = $user->authentication(config('auth.privileges.EnvironmentProtectionLicense'));
         if ($pageAuth['is_create']) {
-            $msg =  \DB::transaction(function () {
-                request()->validate([
-                    'name' => 'required|unique:e_p_l_s,name',
-                    'client_id' => 'required|integer',
-                    'industry_category_id' => 'required|integer',
-                    'contact_no' => ['required', new contactNo],
-                    'address' => ['required'],
-                    'email' => ['sometimes', 'nullable'],
-                    'coordinate_x' => ['numeric', 'nullable', 'between:-180,180'],
-                    'coordinate_y' => ['numeric', 'nullable', 'between:-90,90'],
-                    'pradesheeyasaba_id' => 'required|integer',
-                    'is_industry' => 'required|integer',
-                    'investment' => 'required|numeric',
-                    'start_date' => 'required|date',
-                    'business_scale_id' => 'required|integer',
-                    'registration_no' => ['sometimes', 'nullable', 'unique:e_p_l_s,registration_no'],
-                    'remark' => ['sometimes', 'nullable'],
-                ]);
-                $epl = new EPL();
-                $epl->name = \request('name');
-                $epl->client_id = \request('client_id');
-                $epl->industry_category_id = \request('industry_category_id');
-                $epl->business_scale_id = \request('business_scale_id');
-                $epl->contact_no = \request('contact_no');
-                $epl->address = \request('address');
-                $epl->email = \request('email');
-                $epl->coordinate_x = \request('coordinate_x');
-                $epl->coordinate_y = \request('coordinate_y');
-                $epl->pradesheeyasaba_id = \request('pradesheeyasaba_id');
-                $epl->is_industry = \request('is_industry');
-                $epl->investment = \request('investment');
-                $epl->start_date = \request('start_date');
-                $epl->registration_no = \request('registration_no');
-                $epl->remark = \request('remark');
-                $epl->code = $this->generateCode($epl);
-                $epl->application_path = "";
-                $msg = $epl->save();
+            $msg = \DB::transaction(function () {
+                        request()->validate([
+                            'name' => 'required|unique:e_p_l_s,name',
+                            'client_id' => 'required|integer',
+                            'industry_category_id' => 'required|integer',
+                            'contact_no' => ['required', new contactNo],
+                            'address' => ['required'],
+                            'email' => ['sometimes', 'nullable'],
+                            'coordinate_x' => ['numeric', 'nullable', 'between:-180,180'],
+                            'coordinate_y' => ['numeric', 'nullable', 'between:-90,90'],
+                            'pradesheeyasaba_id' => 'required|integer',
+                            'is_industry' => 'required|integer',
+                            'investment' => 'required|numeric',
+                            'start_date' => 'required|date',
+                            'business_scale_id' => 'required|integer',
+                            'registration_no' => ['sometimes', 'nullable', 'unique:e_p_l_s,registration_no'],
+                            'remark' => ['sometimes', 'nullable'],
+                        ]);
+                        $epl = new EPL();
+                        $epl->name = \request('name');
+                        $epl->client_id = \request('client_id');
+                        $epl->industry_category_id = \request('industry_category_id');
+                        $epl->business_scale_id = \request('business_scale_id');
+                        $epl->contact_no = \request('contact_no');
+                        $epl->address = \request('address');
+                        $epl->email = \request('email');
+                        $epl->coordinate_x = \request('coordinate_x');
+                        $epl->coordinate_y = \request('coordinate_y');
+                        $epl->pradesheeyasaba_id = \request('pradesheeyasaba_id');
+                        $epl->is_industry = \request('is_industry');
+                        $epl->investment = \request('investment');
+                        $epl->start_date = \request('start_date');
+                        $epl->registration_no = \request('registration_no');
+                        $epl->remark = \request('remark');
+                        $epl->code = $this->generateCode($epl);
+                        $epl->application_path = "";
+                        $msg = $epl->save();
 
-                if ($msg) {
-                    $data =  \request('file');
-                    $array = explode(';', $data);
-                    $array2 = explode(',', $array[1]);
-                    $array3 = explode('/', $array[0]);
-                    $type  =  $array3[1];
-                    $data = base64_decode($array2[1]);
-                    file_put_contents($this->makeApplicationPath($epl->id) . "1" . $type, $data);
-                    $epl->application_path = $this->makeApplicationPath($epl->id) . "1." . $type;
-                    $epl->save();
-                    return array('id' => 1, 'message' => 'true', 'rout' => "/epl_profile/client/" . $epl->client_id . "/profile/" . $epl->id);
-                } else {
-                    return array('id' => 0, 'message' => 'false');
-                }
-            });
+                        if ($msg) {
+                            $data = \request('file');
+                            $array = explode(';', $data);
+                            $array2 = explode(',', $array[1]);
+                            $array3 = explode('/', $array[0]);
+                            $type = $array3[1];
+                            $data = base64_decode($array2[1]);
+                            file_put_contents($this->makeApplicationPath($epl->id) . "1" . $type, $data);
+                            $epl->application_path = $this->makeApplicationPath($epl->id) . "1." . $type;
+                            $epl->save();
+                            return array('id' => 1, 'message' => 'true', 'rout' => "/epl_profile/client/" . $epl->client_id . "/profile/" . $epl->id);
+                        } else {
+                            return array('id' => 0, 'message' => 'false');
+                        }
+                    });
             return $msg;
         } else {
             abort(401);
         }
     }
+
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         //
     }
 
-    public function find($id)
-    {
+    public function find($id) {
         return EPL::leftJoin('environment_officers', 'e_p_l_s.environment_officer_id', 'environment_officers.id')
-            ->where('id', $id)
-            ->select('e_p_l_s *', 'environment_officers.name')
-            ->get();
+                        ->leftJoin('users', 'environment_officers.user_id', 'users.id')
+                        ->where('e_p_l_s.id', $id)
+                        ->select('e_p_l_s.*', 'users.first_name','users.last_name')
+                        ->first();
     }
 
     /**
@@ -146,8 +143,7 @@ class EPLController extends Controller
      * @param  \App\EPL  $ePL
      * @return \Illuminate\Http\Response
      */
-    public function show(EPL $ePL)
-    {
+    public function show(EPL $ePL) {
         //
     }
 
@@ -157,8 +153,7 @@ class EPLController extends Controller
      * @param  \App\EPL  $ePL
      * @return \Illuminate\Http\Response
      */
-    public function edit(EPL $ePL)
-    {
+    public function edit(EPL $ePL) {
         //
     }
 
@@ -169,8 +164,7 @@ class EPLController extends Controller
      * @param  \App\EPL  $ePL
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, EPL $ePL)
-    {
+    public function update(Request $request, EPL $ePL) {
         //
     }
 
@@ -180,13 +174,11 @@ class EPLController extends Controller
      * @param  \App\EPL  $ePL
      * @return \Illuminate\Http\Response
      */
-    public function destroy(EPL $ePL)
-    {
+    public function destroy(EPL $ePL) {
         //
     }
 
-    private function generateCode($epl)
-    {
+    private function generateCode($epl) {
         $la = Pradesheeyasaba::find($epl->pradesheeyasaba_id);
         // print_r($la);
         $lsCOde = $la->code;
@@ -206,8 +198,7 @@ class EPLController extends Controller
         return "PEA/" . $lsCOde . "/EPL/" . $industryCode . "/" . $scaleCode . "/" . $serial . "/" . date("Y");
     }
 
-    private function makeApplicationPath($id)
-    {
+    private function makeApplicationPath($id) {
         if (!is_dir("uploads")) {
             //Create our directory if it does not exist
             mkdir("uploads");
@@ -224,11 +215,10 @@ class EPLController extends Controller
             //Create our directory if it does not exist
             mkdir("uploads/EPL/" . $id . "/application");
         }
-        return  "uploads/EPL/" . $id . "/application/";
+        return "uploads/EPL/" . $id . "/application/";
     }
 
-    public function addInspectionPayment()
-    {
+    public function addInspectionPayment() {
         $epl = EPL::find(\request('id'));
         if ($epl !== null) {
             $user = Auth::user();
@@ -243,11 +233,11 @@ class EPLController extends Controller
                 $transaction['payment_type_id'] = Payment::find(\request('payment_id'))->payment_type_id;
                 $transaction['payment_id'] = \request('payment_id');
                 $transaction['transaction_type'] = EPL::EPL;
-                $transaction['transaction_id'] =  $epl->id;
+                $transaction['transaction_id'] = $epl->id;
                 $transaction['amount'] = \request('amount');
                 $transaction['status'] = 0;
                 $transaction['type'] = EPL::INSPECTION;
-                $msg  =  TransactionController::create($transaction);
+                $msg = TransactionController::create($transaction);
                 if ($msg) {
                     return $this->addInspectionFine($epl);
                 } else {
@@ -260,13 +250,13 @@ class EPLController extends Controller
             abort(404);
         }
     }
-    private function addInspectionFine($epl)
-    {
+
+    private function addInspectionFine($epl) {
         if (is_null($epl->site_clearance_file)) {
             $transaction = array();
             if (strtotime($epl->start_date) >= strtotime(EPL::FINEDATE)) {
                 //  after            
-                $transaction['amount'] =  $epl->paymentDetails()['inspection_total'] * 2;
+                $transaction['amount'] = $epl->paymentDetails()['inspection_total'] * 2;
             } else {
                 $baseAmount = 0;
                 $certificateFee = 0;
@@ -318,11 +308,12 @@ class EPLController extends Controller
                 $month1 = date('m', $ts1);
                 $month2 = date('m', $ts2);
 
-                $noOFMonths  = (($year2 - $year1) * 12) + ($month2 - $month1);
+                $noOFMonths = (($year2 - $year1) * 12) + ($month2 - $month1);
                 $transaction['amount'] = $baseAmount * ($noOFMonths - 1) + $certificateFee;
             }
 
-            $pay = Payment::where('name', '=', 'EPL_INSPECTION_FINE')->first();;
+            $pay = Payment::where('name', '=', 'EPL_INSPECTION_FINE')->first();
+            ;
             // dd($pay);
             $transaction['payment_type_id'] = $pay->payment_type_id;
             $transaction['payment_id'] = $pay->id;
@@ -330,7 +321,7 @@ class EPLController extends Controller
             $transaction['transaction_id'] = $epl->id;
             $transaction['status'] = 0;
             $transaction['type'] = EPL::INSPECTION_FINE;
-            $msg  =  TransactionController::create($transaction);
+            $msg = TransactionController::create($transaction);
 
             if ($msg) {
                 return array('id' => 1, 'message' => 'ok', 'amount' => $transaction['amount']);
@@ -341,8 +332,8 @@ class EPLController extends Controller
             return array('id' => 0, 'message' => 'no_fine');
         }
     }
-    public function getInspectionPaymentDetails($epl)
-    {
+
+    public function getInspectionPaymentDetails($epl) {
         $epl = EPL::find($epl);
         if ($epl !== null) {
             return $epl->paymentDetails();
@@ -351,14 +342,14 @@ class EPLController extends Controller
         }
     }
 
-    public function newEpls()
-    {
+    public function newEpls() {
         $user = Auth::user();
         $pageAuth = $user->authentication(config('auth.privileges.EnvironmentProtectionLicense'));
         if ($pageAuth['is_create']) {
-            return  EPL::whereNull('environment_officer_id')->get();
+            return EPL::whereNull('environment_officer_id')->get();
         } else {
             return abort(4010);
         }
     }
+
 }
