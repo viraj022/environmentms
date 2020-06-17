3220047f4dfe5409060be2d5c4340d1996afdad9
hansana <hansanacham@gmail.com>
2020-06-17 10:29:15 +0530

    ok

--- database/migrations/2020_06_16_113014_create_remarks_table.php
+++ database/migrations/2020_06_16_113014_create_remarks_table.php
@@ -15,7 +15,13 @@ class CreateRemarksTable extends Migration
     {
         Schema::create('remarks', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('application_type_id')->unsigned();
+            $table->string('remark');
+            $table->bigInteger('user_id')->unsigned();
+            $table->softDeletes();
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('application_type_id')->references('id')->on('application_types')->onDelete('restrict')->onUpdate('cascade');
         });
     }
 
