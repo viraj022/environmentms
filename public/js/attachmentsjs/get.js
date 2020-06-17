c3e290e5ee94aabc95b9cecdfaf15d3510a84306
viraj <viraj@cetechsystemsolutions.com>
2020-06-17 15:06:52 +0530

    file upload done

--- public/js/attachmentsjs/get.js
+++ public/js/attachmentsjs/get.js
@@ -123,9 +123,9 @@ function all_attachmentsList(epl_id, callBack) {
                 table += "<td>" + ++index + "</td>";
                 table += "<td>" + value.attachment_name + "</td>";
                 if (value.attachment_epl_id == null) {
-                    table += "<td><input type='file' class=\"form-control\" value='" + value.att_id + "'></td>";
+                    table += "<td><input type='file' class=\"form-control fileInput\" id='file_" + value.att_id + "' data-attachment_id='" + value.att_id + "' value='" + value.att_id + "'></td>";
                 } else {
-                    table += "<td></td>";
+                    table += "<td><button type='button' class=\"btn btn-danger removeAttachment\" value=" + value.att_id + ">Remove</button><a href='/" + value.path + "' target=\"_blank\">&nbsp&nbsp (View File)</a></td>";
                 }
                 table += "</tr>";
             });
@@ -139,4 +139,63 @@ function all_attachmentsList(epl_id, callBack) {
         }
     });
 
+}
+function readImage(selected_id, callback) {
+    var img = document.getElementById(selected_id);
+    if (img.files && img.files[0]) {
+        var FR = new FileReader();
+        FR.addEventListener("load", function (e) {
+            //   document.getElementById("b64").innerHTML = e.target.result;
+            callback(e.target.result);
+        });
+        FR.readAsDataURL(img.files[0]);
+    } else {
+        return false;
+//        alert("No Image");
+    }
+}
+
+function saveEPL_Attachment(file_data, epl_id, attachment_id, callBack) {
+    $.ajax({
+        type: "POST",
+        headers: {
+            "Authorization": "Bearer " + $('meta[name=api-token]').attr("content"),
+            "Accept": "application/json"
+        },
+        url: "/api/epl/attachement/set/attachment/" + attachment_id + "/elp/" + epl_id,
+        data: {file: file_data},
+        dataType: "json",
+        cache: false,
+        processDaate: false,
+        success: function (result) {
+            if (typeof callBack !== 'undefined' && callBack != null && typeof callBack === "function") {
+                callBack(result);
+            }
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            alert(textStatus + ':' + errorThrown);
+        }
+    });
+}
+function removeEPL_Attachment(epl_id, attachment_id, callBack) {
+    $.ajax({
+        type: "DELETE",
+        headers: {
+            "Authorization": "Bearer " + $('meta[name=api-token]').attr("content"),
+            "Accept": "application/json"
+        },
+        url: "/api/epl/attachement/unset/attachment/" + attachment_id + "/elp/" + epl_id,
+        data: null,
+        dataType: "json",
+        cache: false,
+        processDaate: false,
+        success: function (result) {
+            if (typeof callBack !== 'undefined' && callBack != null && typeof callBack === "function") {
+                callBack(result);
+            }
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            alert(textStatus + ':' + errorThrown);
+        }
+    });
 }
