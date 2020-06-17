2166681b0e3914401a4983381a6a18268e935ef2
hansana <hansanacham@gmail.com>
2020-06-17 11:37:08 +0530

    ok

---
+++ app/Http/Controllers/InspectionDateLogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\InspectionDateLog;
+use Illuminate\Http\Request;
+
+class InspectionDateLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\InspectionDateLog  $inspectionDateLog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InspectionDateLog $inspectionDateLog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\InspectionDateLog  $inspectionDateLog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InspectionDateLog $inspectionDateLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\InspectionDateLog  $inspectionDateLog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InspectionDateLog $inspectionDateLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\InspectionDateLog  $inspectionDateLog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InspectionDateLog $inspectionDateLog)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/InspectionSessionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\InspectionSession;
+use Illuminate\Http\Request;
+
+class InspectionSessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\InspectionSession  $inspectionSession
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InspectionSession $inspectionSession)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\InspectionSession  $inspectionSession
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InspectionSession $inspectionSession)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\InspectionSession  $inspectionSession
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InspectionSession $inspectionSession)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\InspectionSession  $inspectionSession
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InspectionSession $inspectionSession)
+    {
+        //
+    }
+}
