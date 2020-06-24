01ffec6fc09395461fa7c00710461f02b13f6662
hansana <hansanacham@gmail.com>
2020-06-24 11:45:43 +0530

    ok

--- app/PaymentType.php
+++ app/PaymentType.php
@@ -4,7 +4,6 @@ namespace App;
 
 use Faker\Provider\ar_SA\Payment;
 use Illuminate\Database\Eloquent\Model;
-use App\PaymentType;
 
 class PaymentType extends Model
 {
@@ -13,8 +12,9 @@ class PaymentType extends Model
     {
         return $this->hasMany(Payment::class);
     }
-    
-    public static function getpaymentByTypeName($name){
-        return PaymentType::where('name',$name)->first();
+
+    public static function getpaymentByTypeName($name)
+    {
+        return PaymentType::where('name', $name)->first();
     }
 }
