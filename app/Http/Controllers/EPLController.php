f973a57e4729afe62d38ea5262785592eb17e5dd
hansana <hansanacham@gmail.com>
2020-07-03 14:09:01 +0530

    ok

--- app/Http/Controllers/EPLController.php
+++ app/Http/Controllers/EPLController.php
@@ -83,7 +83,8 @@ class EPLController extends Controller
         }
     }
 
-    public function issue_certificate($epl_id) {
+    public function issue_certificate($epl_id)
+    {
         $user = Auth::user();
         $pageAuth = $user->authentication(config('auth.privileges.EnvironmentProtectionLicense'));
         if ($pageAuth['is_create']) {
@@ -93,37 +94,36 @@ class EPLController extends Controller
                 if ($epl->issue_date == null) {
                     $payList = $epl->paymentList();
                     if (
-                            $payList['inspection']['status'] == 'payed' && $payList['license_fee']['status'] == 'payed' && ($payList['fine']['status'] == 'payed' || $payList['fine']['status'] == 'not_available')
+                        $payList['inspection']['status'] == 'payed' && $payList['license_fee']['status'] == 'payed' && ($payList['fine']['status'] == 'payed' || $payList['fine']['status'] == 'not_available')
                     ) {
-
                         request()->validate([
                             'issue_date' => 'required|date',
                             'expire_date' => 'required|date',
                             'certificate_no' => 'required|string',
                         ]);
                         return \DB::transaction(function () use ($epl, $user) {
-                                    $epl->issue_date = request('issue_date');
-                                    $epl->expire_date = request('expire_date');
-                                    $epl->certificate_no = request('certificate_no');
-                                    $msg = $epl->save();
-                                    if ($msg) {
-                                        $issueLog = new IssueLog();
-                                        $issueLog->certificate_type = IssueLog::CER_EPL;
-                                        $issueLog->issue_type = IssueLog::CER_EPL;
-                                        $issueLog->issue_id = $epl->id;
-                                        $issueLog->issue_date = request('issue_date');
-                                        $issueLog->expire_date = request('expire_date');
-                                        $issueLog->user_id = $user->id;
-                                        $msg = $issueLog->save();
-                                        if ($msg) {
-                                            return response(array('id' => 1, 'message' => 'success'), 200);
-                                        } else {
-                                            return response(array('id' => 0, 'message' => 'fail'), 200);
-                                        }
-                                    } else {
-                                        abort(500);
-                                    }
-                                });
+                            $epl->issue_date = request('issue_date');
+                            $epl->expire_date = request('expire_date');
+                            $epl->certificate_no = request('certificate_no');
+                            $msg = $epl->save();
+                            if ($msg) {
+                                $issueLog = new IssueLog();
+                                $issueLog->certificate_type = IssueLog::CER_EPL;
+                                $issueLog->issue_type = IssueLog::CER_EPL;
+                                $issueLog->issue_id = $epl->id;
+                                $issueLog->issue_date = request('issue_date');
+                                $issueLog->expire_date = request('expire_date');
+                                $issueLog->user_id = $user->id;
+                                $msg = $issueLog->save();
+                                if ($msg) {
+                                    return response(array('id' => 1, 'message' => 'success'), 200);
+                                } else {
+                                    return response(array('id' => 0, 'message' => 'fail'), 200);
+                                }
+                            } else {
+                                abort(500);
+                            }
+                        });
                     } else {
                         return response(array('id' => 0, 'message' => 'Payment no completed'), 403);
                     }
