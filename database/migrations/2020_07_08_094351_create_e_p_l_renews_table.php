66a22ee99d8d5585e5ca61d3d8e74eb5453ef2c0
hansana <hansanacham@gmail.com>
2020-07-09 11:39:08 +0530

    ok

--- database/migrations/2020_07_08_094351_create_e_p_l_renews_table.php
+++ database/migrations/2020_07_08_094351_create_e_p_l_renews_table.php
@@ -17,8 +17,11 @@ class CreateEPLRenewsTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('e_p_l_id')->unsigned();
             $table->integer('count');
-            $table->integer('is_old');
-            $table->integer('is_old');
+            $table->integer('is_old')->default(0)->comment('0 for old 1 for new');
+            $table->string('certificate_no', 100);
+            $table->dateTime('renew_date')->nullable();
+            $table->dateTime('expire_date')->nullable();
+            $table->integer('issue_status')->default(0);
             $table->timestamps();
         });
     }
