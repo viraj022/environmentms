b9e083fc80c558a3ab85146a5a1151a6b23acf46
Jake Logan <castleofae@gmail.com>
2020-07-27 16:57:49 +0530

    modify client space page and epl register page

--- resources/views/client_space.blade.php
+++ resources/views/client_space.blade.php
@@ -32,67 +32,150 @@
         <div class="row">
 
             <div class="col-md-12">
-                <div class="col-md-9">
-                    <div class="card card-primary">
-                        <div class="card-header">
-                            <label id="lblTitle">Register New Client</label>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <label id="lblTitle">Register New Client</label>
+                            </div>
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label>First Name*</label>
+                                    <input id="getfName" type="text" class="form-control form-control-sm"
+                                           placeholder="Enter Name..."
+                                           value="">
+                                    <div id="valName" class="d-none"><p class="text-danger">Name is required</p></div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Last Name*</label>
+                                    <input id="getlName" type="text" class="form-control form-control-sm"
+                                           placeholder="Enter Name..."
+                                           value="">
+                                    <div id="valLName" class="d-none"><p class="text-danger">Last Name is required</p></div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Address</label>
+                                    <input id="getAddress" type="text" class="form-control form-control-sm"
+                                           placeholder="Enter Name..."
+                                           value="">
+                                    <div id="valAddName" class="d-none"><p class="text-danger">Address is required</p></div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Contact Number</label>
+                                    <input id="getContact" type="text" class="form-control form-control-sm"
+                                           placeholder="Enter Name..."
+                                           value="">
+                                    <div id="valConName" class="d-none"><p class="text-danger">Contact Number is required</p></div>
+                                </div>
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input id="getEmail" type="text" class="form-control form-control-sm"
+                                           placeholder="Enter Name..."
+                                           value="">
+                                    <div id="valNam1e" class="d-none"><p class="text-danger">Email is required</p></div>
+                                </div>
+                                <div class="form-group">
+                                    <label>NIC</label>
+                                    <input id="getNicSave" type="text" class="form-control form-control-sm"
+                                           placeholder="Enter Name..."
+                                           value="">
+                                    <div id="valnicName" class="d-none"><p class="text-danger">NIC is required</p></div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                @if($pageAuth['is_create']==1 || false)
+                                <button id="btnSave" type="submit" class="btn btn-success">Register</button>
+                                @endif
+                                @if($pageAuth['is_update']==1 || false)
+                                <button id="btnUpdate" type="submit" class="btn btn-warning d-none">Update</button>
+                                @endif
+                                @if($pageAuth['is_delete']==1 || false)
+                                <button  id="btnshowDelete" type="submit" class="btn btn-danger d-none"  data-toggle="modal"
+                                         data-target="#modal-danger">Delete</button>
+                                @endif
+                            </div>                           
                         </div>
-                        <div class="card-body">
-                            <div class="form-group">
-                                <label>First Name*</label>
-                                <input id="getfName" type="text" class="form-control form-control-sm"
-                                       placeholder="Enter Name..."
-                                       value="">
-                                <div id="valName" class="d-none"><p class="text-danger">Name is required</p></div>
-                            </div>
-                            <div class="form-group">
-                                <label>Last Name*</label>
-                                <input id="getlName" type="text" class="form-control form-control-sm"
-                                       placeholder="Enter Name..."
-                                       value="">
-                                <div id="valLName" class="d-none"><p class="text-danger">Last Name is required</p></div>
-                            </div>
-                            <div class="form-group">
-                                <label>Address</label>
-                                <input id="getAddress" type="text" class="form-control form-control-sm"
-                                       placeholder="Enter Name..."
-                                       value="">
-                                <div id="valAddName" class="d-none"><p class="text-danger">Address is required</p></div>
-                            </div>
-                            <div class="form-group">
-                                <label>Contact Number</label>
-                                <input id="getContact" type="text" class="form-control form-control-sm"
-                                       placeholder="Enter Name..."
-                                       value="">
-                                <div id="valConName" class="d-none"><p class="text-danger">Contact Number is required</p></div>
-                            </div>
-                            <div class="form-group">
-                                <label>Email</label>
-                                <input id="getEmail" type="text" class="form-control form-control-sm"
-                                       placeholder="Enter Name..."
-                                       value="">
-                                <div id="valNam1e" class="d-none"><p class="text-danger">Email is required</p></div>
-                            </div>
-                            <div class="form-group">
-                                <label>NIC</label>
-                                <input id="getNicSave" type="text" class="form-control form-control-sm"
-                                       placeholder="Enter Name..."
-                                       value="">
-                                <div id="valnicName" class="d-none"><p class="text-danger">NIC is required</p></div>
+                        <div class="">
+                            <div class="card card-primary">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="card">
+                                                <div class="card-header">
+                                                    <label id="lblTitle">Map</label>
+                                                </div>
+                                                <!-- /.card-header -->
+                                                <div class="card-body p-0">
+                                                    <div id="map" style="width: 100%; height: 600px;"></div>
+                                                </div>
+                                                <!-- /.card-body -->
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="card-footer">
-                            @if($pageAuth['is_create']==1 || false)
-                            <button id="btnSave" type="submit" class="btn btn-success">Register</button>
-                            @endif
-                            @if($pageAuth['is_update']==1 || false)
-                            <button id="btnUpdate" type="submit" class="btn btn-warning d-none">Update</button>
-                            @endif
-                            @if($pageAuth['is_delete']==1 || false)
-                            <button  id="btnshowDelete" type="submit" class="btn btn-danger d-none"  data-toggle="modal"
-                                     data-target="#modal-danger">Delete</button>
-                            @endif
-                        </div>                           
+                    </div>
+                    <!--Industry details-->
+                    <div class="col-md-6">
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <label id="lblTitle">Industry details</label>
+                            </div>
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label>Pradeshiya Sabha*</label>
+                                    <select id="prsdeshiySb" class="form-control form-control-sm" style="width: 100%;"></select>
+                                </div>
+                                <div class="form-group">
+                                    <label>Industry Category*</label>
+                                    <select id="industryCat" class="form-control form-control-sm" style="width: 100%;"></select>
+                                </div>
+                                <div class="form-group">
+                                    <label>Business Scale*</label>
+                                    <select id="businesScale" class="form-control form-control-sm" style="width: 100%;"></select>
+                                </div>
+                                <div class="form-group">
+                                    <label>Business Registration Number*</label>
+                                    <input id="business_regno" type="text" class="form-control form-control-sm" placeholder="Enter Number" value="">
+                                </div>
+                                <div class="form-group">
+                                    <label>Business Name*</label>
+                                    <input id="business_name" type="text" class="form-control form-control-sm" placeholder="Enter Name..." value="">
+                                </div>
+                                <div class="form-group">
+                                    <label>Is this Industry</label>
+                                    <select id="getZone" class="form-control form-control-sm" style="width: 100%;">
+                                        <option value="0">No</option>
+                                        <option value="1">Yes</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label>Investment*</label>
+                                    <input id="inventsment" type="number" class="form-control form-control-sm" placeholder="Enter investment" value="">
+                                </div>
+                                <div class="form-group">
+                                    <label>Address*</label>
+                                    <input id="getAddressT" type="text" class="form-control form-control-sm" placeholder="Enter Name..." value="">
+                                </div>
+                                <div class="form-group">
+                                    <label>Start Date*</label>
+                                    <input id="startDate" type="date" class="form-control form-control-sm" placeholder="Enter Name..." value="">
+                                </div>
+                                <div class="form-group">
+                                    <label>Submitted Date*</label>
+                                    <input id="submittedDate" type="date" class="form-control form-control-sm" placeholder="Enter Name..." value="">
+                                </div>
+                                <div class="form-group">
+                                    <label>Contact No*</label>
+                                    <input id="getContactn" type="text" class="form-control form-control-sm" placeholder="Enter Name..." value="">
+                                </div>
+                                <div class="form-group">
+                                    <label>Email*</label>
+                                    <input id="getEmailI" type="text" class="form-control form-control-sm" placeholder="Enter Name..." value="">
+                                </div>
+                            </div>                        
+                        </div>
                     </div>
                 </div>
                 <!--If something about Datatable place tt here again! -->
@@ -336,16 +419,41 @@
 <script src="../../dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="../../js/ClientJS/client_space.js"></script>
+<script src="../../js/epl/epl_register.js"></script>
 <script src="../../js/ClientJS/submit.js"></script>
 <script src="../../js/ClientJS/get.js"></script>
 <script src="../../js/ClientJS/update.js"></script>
 <script src="../../js/ClientJS/delete.js"></script>
 <script src="../../js/ClientJS/viewClientData.js"></script>
 <!-- AdminLTE App -->
+<script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDyaUNtnrMrJwLqWQmHoUbeHaLk6q4msXE&callback=initMap"></script>
 <script>
-    $(function () {
+var _Latitude = 7.489050;
+var _Longitude = 80.349985;
+// Initialize and add the map
+function initMap() {
+    // The location of CeyTech
+    var defaultLocation = {lat: 7.489050, lng: 80.349985}; //default Location for load map
+
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 15, center: defaultLocation});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: defaultLocation, map: map, draggable: true,
+        title: "Drag me!"});
+    google.maps.event.addListener(marker, 'dragend', function (evt) {
+        _Latitude = evt.latLng.lat().toFixed(6); //change  decimal point if have problam with location accuracy
+        _Longitude = evt.latLng.lng().toFixed(6); //change  decimal point if have problam with location accuracy
+    });
+}    
+           
+    
+$(function () {
 //Load table
         loadTable();
+        loadPradeshiyaSabha();
+        IndustryCategoryCombo();
+        BusinessScaleCombo();
 //Register Button
         $('#btnSave').click(function () {
             var data = fromValues();
@@ -535,7 +643,7 @@
         });
     });
 
-    //btnCustomerVa button action 
+//btnCustomerVa button action 
     $(document).on('click', '.btnCustomerVa', function () {
         var row = JSON.parse(decodeURIComponent($(this).data('row')));
         setClientDetails(row);
