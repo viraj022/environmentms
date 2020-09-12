68c722364fdc48dc6677bd6a9475b75dcdfcb8db
Jake Logan <castleofae@gmail.com>
2020-09-12 22:17:53 +0530

    director pendig list

---
+++ public/js/DirectorPendingJS/director_pending_list.js
@@ -0,0 +1,66 @@
+function loadAssistantDirectorCombo(callBack) { // Load Assustant Direcotr Combo
+    ajaxRequest('GET', "/api/AssistantDirector/active", null, function (dataSet) {
+        var combo = "";
+        if (dataSet.length == 0) {
+            combo += "<option value=''>NO DATA FOUND</option>";
+        } else {
+            $.each(dataSet, function (index, value) {
+                combo += "<option value='" + value.id + "'>" + value.first_name + ' ' + value.last_name + "</option>";
+            });
+        }
+        $('.combo_AssistantDirector').html(combo);
+        if (typeof callBack !== 'undefined' && callBack != null && typeof callBack === "function") {
+            callBack();
+        }
+    });
+}
+
+function getAdPendingList(id, callBack) {
+    if (id.length == 0) {
+        return false;
+    }
+    var url = "/api/files/pending/assistance_director/" + id;
+    ajaxRequest('GET', url, null, function (result) {
+        if (typeof callBack !== 'undefined' && callBack !== null && typeof callBack === "function") {
+            callBack(result);
+        }
+    });
+}
+
+function loadAdPendingListTable(id) {
+    getAdPendingList(id, function (result) {
+        var dataObj = {0: 'pending', 1: 'AD File Approval Pending', 2: 'Certificate Preparation', 3: 'AD Certificate Prenidng Approval', 4: 'D Certificate Approval Prenidng', 5: 'Complete', 6: 'Issued', '-1': 'Rejected', '-2': 'Hold'};
+        var tbl = "";
+        var id = 1;
+        if (result.length == 0) {
+            tbl = "<tr><td colspan='4'>No Data Found</td></tr>";
+        } else {
+            $.each(result, function (index, row) {
+                tbl += '<tr>';
+                tbl += '<td>' + ++index + '</td>';
+                tbl += '<td>' + row.industry_name + '</td>';
+                tbl += '<td><a href="/industry_profile/id/' + row.id + '" target="_blank">' + row.file_no + '</a></td>';
+                tbl += '<td>' + dataObj[row.file_status] + '</td>';
+                if (row.file_status != 0) {
+                    tbl += '<td><button value="' + escape(JSON.stringify(row)) + '" class="btn btn-success actionDetails">Action</button></td>';
+                } else {
+                    tbl += '<td>N/A</td>';
+                }
+                tbl += '</tr>';
+            });
+        }
+        $('#tblPendingAdList tbody').html(tbl);
+    });
+}
+
+function preCertificateApi(file_id, assDir_id, callBack) {
+    if (file_id.length == 0) {
+        return false;
+    }
+    var url = "/api/assistant_director/approve/" + assDir_id + "/" + file_id;
+    ajaxRequest('PATCH', url, null, function (result) {
+        if (typeof callBack !== 'undefined' && callBack !== null && typeof callBack === "function") {
+            callBack(result);
+        }
+    });
+}
---
+++ resources/views/director_pending_list.blade.php
@@ -0,0 +1,149 @@
+@extends('layouts.admin')
+@extends('layouts.styles')
+@extends('layouts.scripts')
+@extends('layouts.navbar')
+@extends('layouts.sidebar')
+@extends('layouts.footer')
+@section('pageStyles')
+<!-- Select2 -->
+<link rel="stylesheet" href="/plugins/select2/css/select2.min.css">
+<link rel="stylesheet" href="/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+<!-- Bootstrap4 Duallistbox -->
+<link rel="stylesheet" href="/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="/dist/css/adminlte.min.css">
+<!-- Google Font: Source Sans Pro -->
+@endsection
+@section('content')
+@if($pageAuth['is_read']==1 || false)
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-12 col-sm-6">
+                <h1>Director Pending List</h1>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="form-group" id="comboAssistantDirectorDisplay">
+                            <label>Assistant Director*</label>
+                            <select id="getAssistantDirector" class="form-control form-control-sm combo_AssistantDirector" style="width: 100%;" ></select>
+                            <div id="valZone" class="d-none"><p class="text-danger">Field is required</p></div>
+                        </div>
+                    </div>
+                    <div class="card-body p-0">
+                        <div class="card-body table-responsive" style="height: 450px;">
+                            <table class="table table-condensed" id="tblPendingAdList">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 10px">#</th>
+                                        <th>Industry Name</th>
+                                        <th>File No</th>
+                                        <th>Status</th>
+                                        <th style="width: 140px">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="modal-x2">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="modalTitlex2">Modal</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <button type="button" id="prepareCertificate" class="btn btn-primary d-none"><i class="fa fa-check"></i> Approve For Prepare Certificate</button>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endif
+@endsection
+
+@section('pageScripts')
+<!-- Page script -->
+<!-- Select2 -->
+<script src="../../plugins/select2/js/select2.full.min.js"></script>
+<!-- Bootstrap4 Duallistbox -->
+<script src="../../plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+<!-- InputMask -->
+<script src="../../plugins/moment/moment.min.js"></script>
+<script src="../../plugins/inputmask/min/jquery.inputmask.bundle.min.js"></script>
+<!-- date-range-picker -->
+<script src="../../plugins/daterangepicker/daterangepicker.js"></script>
+<!-- bootstrap color picker -->
+<script src="../../plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="../../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Bootstrap Switch -->
+<script src="../../plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+<script src="../../js/DirectorPendingJS/director_pending_list.js" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script>
+    $(function () {
+//Load table
+        loadAssistantDirectorCombo(function () {
+            loadAdPendingListTable($('#getAssistantDirector').val());
+        });
+
+        $('#getAssistantDirector').change(function () {
+            loadAdPendingListTable($('#getAssistantDirector').val());
+        });
+//select button action 
+        $(document).on('click', '.btnAction', function () {
+        });
+
+
+        $(document).on('click', '.actionDetails', function () {
+            $('#modal-x2').modal();
+            var fileData = JSON.parse(unescape($(this).val()));
+            $('#prepareCertificate').val($(this).val()); //<-- Share this button value to this button
+            let f_Status = fileData.file_status;
+            $('#modalTitlex2').html(fileData.file_no);
+            if (f_Status == 1) {
+                $('#prepareCertificate').removeClass('d-none');
+            } else {
+                $('#prepareCertificate').addClass('d-none');
+            }
+        });
+
+        $(document).on('click', '#prepareCertificate', function () {
+            var fileData = JSON.parse(unescape($(this).val()));
+            if (confirm('Are you sure you want to approve?')) {
+                preCertificateApi(fileData.id, $('#getAssistantDirector').val(), function (resp) {
+                    show_mesege(resp);
+                    if (resp.id == 1) {
+                        loadAdPendingListTable($('#getAssistantDirector').val());
+                        $('#modal-x2').modal('hide');
+                    }
+                });
+            }
+        });
+
+    });
+
+</script>
+@endsection
