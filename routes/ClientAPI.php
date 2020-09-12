9b56aff468c60b52a196e42d28209e88908a0fbd
hansana <hansana@ceytechsystemsolutions.com>
2020-09-12 20:21:17 +0530

    ok

--- routes/ClientAPI.php
+++ routes/ClientAPI.php
@@ -416,4 +416,4 @@ Route::middleware('auth:api')->post('/certificate/draft/{id}', 'ClientController
 Route::middleware('auth:api')->post('/certificate/original/{id}', 'ClientController@uploadOriginalCertificate'); //  upload certificate original
 
 
-Route::middleware('auth:api')->patch('/certificate/drafted/{id}', 'ClientController@completeDraftingCertificate'); //  make complete
+Route::middleware('auth:api')->patch('/certificate/drafted/{id}', 'ClientController@completeDraftingCertificate'); //  make certificate drafted
