74b971b1fcfd89491e67f31b78fb5d3b97c409ef
Jake Logan <admin@bitbucket.com>
2020-09-23 03:50:46 -0700

    upload file commits and remark done

---
+++ app/Http/Controllers/WebRouteController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\SiteClearenceSession;
+use App\Committee;
+use App\Client;
+
+class WebRouteController extends Controller {
+
+    public function __construct() {
+        $this->middleware(['auth']);
+    }
+
+    public function indexCommittee($id) {
+        $user = Auth::user();
+        $pageAuth = $user->authentication(config('auth.privileges.clientSpace'));
+        $siteclear = SiteClearenceSession::findOrFail($id);
+        $client = Client::find($siteclear->client_id);
+        return view('committee', ['pageAuth' => $pageAuth, 'id' => $id, 'client' => $siteclear->client_id, 'file_no' => $client->file_no]);
+    }
+
+    public function indexCommitteeRemarks($id) {
+        $user = Auth::user();
+        $pageAuth = $user->authentication(config('auth.privileges.clientSpace'));
+        Committee::findOrFail($id);
+        return view('committee_remarks', ['pageAuth' => $pageAuth, 'id' => $id]);
+    }
+
+}
--- public/js/SiteClearanceJS/site_clearance_scr.js
+++ public/js/SiteClearanceJS/site_clearance_scr.js
@@ -1,10 +1,22 @@
 //Get Site Clear By ID
 function getSiteClearanceAPI(id, callBack) {
     if (isNaN(id)) {
-       return false;
+        return false;
     }
     var url = "/api/site_clearance/" + id;
     ajaxRequest('GET', url, null, function (dataSet) {
+        if (typeof callBack !== 'undefined' && callBack != null && typeof callBack === "function") {
+            callBack(dataSet);
+        }
+    });
+}
+//Set Processing Type
+function setProcessingTypeAPI(site_clear_ses,data, callBack) {
+    if (isNaN(site_clear_ses)) {
+        return false;
+    }
+    var url = "/api/site_clearance/processing_status/" + site_clear_ses;
+    ajaxRequest('PATCH', url, data, function (dataSet) {
         if (typeof callBack !== 'undefined' && callBack != null && typeof callBack === "function") {
             callBack(dataSet);
         }
