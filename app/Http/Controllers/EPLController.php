ee424e66a3c26fe37a6a7bb89abfdaccb944ccbf
hansana <hansana@ceytechsystemsolutions.com>
2020-10-12 09:57:59 +0530

    ok

--- app/Http/Controllers/EPLController.php
+++ app/Http/Controllers/EPLController.php
@@ -568,7 +568,7 @@ class EPLController extends Controller
             'expire_date' => 'required|date',
             'certificate_no' => 'required|string',
             'count' => 'required|integer',
-            'submitted_date' => 'required|date',
+            'submit_date' => 'required|date',
             'file' => 'required|mimes:jpeg,jpg,png,pdf'
         ]);
         // save epl main file      
@@ -589,7 +589,7 @@ class EPLController extends Controller
             $epl->certificate_no = \request('certificate_no');
             $epl->status = 1;
             $epl->count = \request('count');
-            $epl->submitted_date = \request('submitted_date');
+            $epl->submitted_date = \request('submit_date');
             $msg = $epl->save();
             if ($msg) {
                 if ($request->file('file') != null) {
@@ -654,7 +654,7 @@ class EPLController extends Controller
             'expire_date' => 'required|date',
             'certificate_no' => 'required|string',
             'count' => 'required|integer',
-            'submitted_date' => 'required|date',
+            'submit_date' => 'required|date',
             'file' => 'sometimes|nullable|mimes:jpeg,jpg,png,pdf'
         ]);
         // save epl main file      
@@ -667,7 +667,7 @@ class EPLController extends Controller
             $epl->expire_date = \request('expire_date');
             $epl->certificate_no = \request('certificate_no');
             $epl->count = \request('count');
-            $epl->submitted_date = \request('submitted_date');
+            $epl->submitted_date = \request('submit_date');
             $msg = $msg && $epl->save();
             // save old data file
             if ($msg) {
