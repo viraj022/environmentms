b69f7db950387fa58f0185bff4633dc1e53f879b
hansana <hansana@ceytechsystemsolutions.com>
2020-10-14 15:55:36 +0530

    ok

--- app/Client.php
+++ app/Client.php
@@ -105,7 +105,7 @@ class Client extends Model
                                 return $data . "/" . date("Y");
                         case 2: //epl renewal
                                 $epl = EPL::Where('client_id', $this->id)->first();
-                                dd($epl->toArray());
+                                // dd($epl->toArray());
                                 $serial_no =  Str::substr($epl->certificate_no, 0, strpos($epl->certificate_no, '/'));
                                 $data = str_pad($serial_no, 6, "0", STR_PAD_LEFT);
                                 return  $data . "/" . date("Y") . "/r" . $epl->count;
