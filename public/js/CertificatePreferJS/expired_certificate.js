381af617d6b413e5f494d42371f96a67a23a0377
viraj <viraj@ceytechsystemsolutions.com>
2021-01-05 17:38:02 +0530

    hot fix

--- public/js/CertificatePreferJS/expired_certificate.js
+++ public/js/CertificatePreferJS/expired_certificate.js
@@ -47,9 +47,9 @@ function getExpireCerByAssDir(id, callBack) {
     if (id != null) {
         url = "/api/certificate/expiredCertificates/id/" + id;
     }
-    console.log(url);
-    var certificate_status = {0: 'pending', 1: 'Drafting', 2: 'Drafted', 3: 'AD Pending', 4: 'Director Pending', 5: 'Director Approved', 6: 'Issued', '-1': 'Hold'};
-    var certificate_type = {0: 'pending', 1: 'New EPL', 2: 'Renew EPL', 3: 'New Site Clearance', 4: 'Site Clearance Extended'};
+//    console.log(url);
+//    var certificate_status = {0: 'pending', 1: 'Drafting', 2: 'Drafted', 3: 'AD Pending', 4: 'Director Pending', 5: 'Director Approved', 6: 'Issued', '-1': 'Hold'};
+//    var certificate_type = {0: 'pending', 1: 'New EPL', 2: 'Renew EPL', 3: 'New Site Clearance', 4: 'Site Clearance Extended'};
     ajaxRequest('GET', url, null, function (result) {
         var tbl = '';
         if (result.length == 0) {
@@ -58,7 +58,7 @@ function getExpireCerByAssDir(id, callBack) {
             $.each(result, function (index, row) {
                 tbl += '<td>' + ++index + '</td>';
                 tbl += '<td>' + row.industry_name + '</td>';
-                tbl += '<td><a href="/industry_profile/id/' + row.id + '" target="_blank">' + row.file_no + '</a>(' + certificate_type[row.cer_type_status] + ')</td>';
+                tbl += '<td><a href="/industry_profile/id/' + row.id + '" target="_blank">' + row.client.file_no + '</a>(' + row.due_date + ')</td>';
                 tbl += '<td><a href="/certificate_perforation/id/' + row.id + '" class="btn btn-success">Certificate<a></td>';
                 tbl += '</tr>';
             });
