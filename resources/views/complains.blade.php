fe496459705c212eb24b3e44bd05e31fe0077a41
kasun clement <kasun@ceytechsystemsolutions.com>
2022-03-21 18:36:25 +0530

    code format 7

--- resources/views/complains.blade.php
+++ resources/views/complains.blade.php
@@ -125,157 +125,157 @@
 @endsection
 
 @section('pageScripts')
-<script src="../../dist/js/adminlte.min.js"></script>
+    <script src="../../dist/js/adminlte.min.js"></script>
 
-<script src="{{ asset('plugins/jquery-validation/jquery.validate.min.js') }}"></script>
-<script src="{{ asset('js/complains/complains.js') }}"></script>
-<script src="../../plugins/select2/js/select2.full.min.js"></script>
-<script src="../../dist/js/adminlte.min.js"></script>
+    <script src="{{ asset('plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('js/complains/complains.js') }}"></script>
+    <script src="../../plugins/select2/js/select2.full.min.js"></script>
+    <script src="../../dist/js/adminlte.min.js"></script>
 
-<!-- Page script -->
-<script>
-    $(document).ready(function() {
-        loadPradeshiyaSabha(function() {
+    <!-- Page script -->
+    <script>
+        $(document).ready(function() {
+            loadPradeshiyaSabha(function() {
+                gen_complain_code();
+            });
+
+            load_complains();
+        });
+
+        $('#ps').change(function() {
             gen_complain_code();
         });
 
-        load_complains();
-    });
-
-    $('#ps').change(function() {
-        gen_complain_code();
-    });
-
-    $('#complain_attach').change(function() {
-        let index = 0;
-        let html = "";
-        $.each($('#complain_attach')[0].files, function(key, val) {
-            html += "<img src='" + window.URL.createObjectURL(val) + "' width='100em' height='100em'/>";
-        });
-        $('#attachment_view').html(html);
-    });
-
-    $('#save').click(function() {
-        let is_valid = jQuery("#complain_frm").valid();
-        if (is_valid) {
-            save_complain();
-        } else {
-            swal.fire('failed', 'Required fields must fill', 'warning');
-        }
-
-    });
-
-    $('#update').click(function() {
-        let is_valid = jQuery("#complain_frm").valid();
-        if (is_valid) {
-            update_complain();
-        } else {
-            swal.fire('failed', 'Required fields must fill', 'warning');
-        }
-    });
-
-    $(document).on('click', '.btn-edit', function() {
-        let id = $(this).val();
-        let url = '/api/complain_profile_data/id/' + id;
-        ajaxRequest('GET', url, null, function(resp) {
-            $('#complainer_code').val(resp.complainer_code);
-            $('#complainer_name_ipt').val(resp.complainer_name);
-            $('#complainer_address_ipt').val(resp.complainer_address);
-            $('#contact_complainer_ipt').val(resp.comp_contact_no);
-            $('#complain_desc_ipt').val(resp.complain_des);
-            $('#recieve_type_ipt').val(resp.recieve_type);
-            $('#hidden_id').val(resp.id);
-            $('#ps').val(resp.pradeshiya_saba_id);
-            $('#ps').change();
-            $('.select2').select2();
+        $('#complain_attach').change(function() {
+            let index = 0;
+            let html = "";
+            $.each($('#complain_attach')[0].files, function(key, val) {
+                html += "<img src='" + window.URL.createObjectURL(val) + "' width='100em' height='100em'/>";
+            });
+            $('#attachment_view').html(html);
         });
 
-        $('#save').addClass('d-none');
-        $('#update').removeClass('d-none');
-    });
-
-    $(document).on('click', '.btn-del', function() {
-
-        let id = $(this).val();
-
-        Swal.fire({
-            title: 'Do you want to delete complain?',
-            showDenyButton: true,
-            showCancelButton: true,
-            confirmButtonText: 'confirm',
-            denyButtonText: `Don't confirm`,
-        }).then((result) => {
-            /* Read more about isConfirmed, isDenied below */
-            if (result.value) {
-                delete_complain(id);
-            } else if (result.isDenied) {
-                Swal.fire('Canceled!', 'Confirmation was cancelled', 'info')
-            }
-        })
-    });
-
-    var complain_form;
-    complain_form = $("#complain_frm").validate({
-        errorClass: "invalid",
-        rules: {
-            contact_complainer_ipt: {
-                valid_lk_phone: true,
-            },
-        },
-        errorElement: 'span',
-        errorPlacement: function(error, element) {
-            error.addClass('invalid-feedback');
-            element.closest('.form-group').append(error);
-        },
-        highlight: function(element, errorClass, validClass) {
-            $(element).addClass('is-invalid');
-        },
-        unhighlight: function(element, errorClass, validClass) {
-            $(element).removeClass('is-invalid');
-        }
-    });
-
-    jQuery.validator.setDefaults({
-        errorElement: "span",
-        ignore: ":hidden:not(select.chosen-select)",
-        errorPlacement: function(error, element) {
-            // Add the `help-block` class to the error element
-            error.addClass("help-block");
-            if (element.prop("type") === "checkbox") {
-                //                error.insertAfter(element.parent("label"));
-                error.appendTo(element.parents("validate-parent"));
-            } else if (element.is("select.chosen-select")) {
-                error.insertAfter(element.siblings(".chosen-container"));
-            } else if (element.prop("type") === "radio") {
-                error.appendTo(element.parents("div.validate-parent"));
+        $('#save').click(function() {
+            let is_valid = jQuery("#complain_frm").valid();
+            if (is_valid) {
+                save_complain();
             } else {
-                error.insertAfter(element);
+                swal.fire('failed', 'Required fields must fill', 'warning');
             }
-        },
-        highlight: function(element, errorClass, validClass) {
-            jQuery(element).parents(".validate-parent").addClass("has-error").removeClass("has-success");
-        },
-        unhighlight: function(element, errorClass, validClass) {
-            jQuery(element).parents(".validate-parent").removeClass("has-error");
-        }
-    });
-    jQuery.validator.addMethod("valide_code", function(value, element) {
-        return this.optional(element) || /^[a-zA-Z\s\d\_\-()]{1,100}$/.test(value);
-    }, "Please enter a valid Code");
-    jQuery.validator.addMethod("valid_name", function(value, element) {
-        return this.optional(element) || /^[a-zA-Z\s\.\&\-()]*$/.test(value);
-    }, "Please enter a valid name");
-    jQuery.validator.addMethod("valid_date", function(value, element) {
-        return this.optional(element) || /^\d{4}\-\d{2}\-\d{2}$/.test(value);
-    }, "Please enter a valid date ex. 2017-03-27");
-    jQuery.validator.addMethod("valide_email", function(value, element) {
-        return this.optional(element) || /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/.test(value);
-    }, "Please enter a valid email addresss");
-    jQuery.validator.addMethod("valid_lk_phone", function(value, element) {
-        return this.optional(element) || /^0[7][0-9]{8}$/.test(value);
-    }, "Please enter a valid phone number");
-    jQuery.validator.addMethod("valid_lk_nic", function(value, element) {
-        return this.optional(element) || /^([0-9]{9}[x|X|v|V]|[0-9]{12})$/.test(value);
-    }, "Please enter a valid NIC number");
+
+        });
+
+        $('#update').click(function() {
+            let is_valid = jQuery("#complain_frm").valid();
+            if (is_valid) {
+                update_complain();
+            } else {
+                swal.fire('failed', 'Required fields must fill', 'warning');
+            }
+        });
+
+        $(document).on('click', '.btn-edit', function() {
+            let id = $(this).val();
+            let url = '/api/complain_profile_data/id/' + id;
+            ajaxRequest('GET', url, null, function(resp) {
+                $('#complainer_code').val(resp.complainer_code);
+                $('#complainer_name_ipt').val(resp.complainer_name);
+                $('#complainer_address_ipt').val(resp.complainer_address);
+                $('#contact_complainer_ipt').val(resp.comp_contact_no);
+                $('#complain_desc_ipt').val(resp.complain_des);
+                $('#recieve_type_ipt').val(resp.recieve_type);
+                $('#hidden_id').val(resp.id);
+                $('#ps').val(resp.pradeshiya_saba_id);
+                $('#ps').change();
+                $('.select2').select2();
+            });
+
+            $('#save').addClass('d-none');
+            $('#update').removeClass('d-none');
+        });
+
+        $(document).on('click', '.btn-del', function() {
+
+            let id = $(this).val();
+
+            Swal.fire({
+                title: 'Do you want to delete complain?',
+                showDenyButton: true,
+                showCancelButton: true,
+                confirmButtonText: 'confirm',
+                denyButtonText: `Don't confirm`,
+            }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.value) {
+                    delete_complain(id);
+                } else if (result.isDenied) {
+                    Swal.fire('Canceled!', 'Confirmation was cancelled', 'info')
+                }
+            })
+        });
+
+        var complain_form;
+        complain_form = $("#complain_frm").validate({
+            errorClass: "invalid",
+            rules: {
+                contact_complainer_ipt: {
+                    valid_lk_phone: true,
+                },
+            },
+            errorElement: 'span',
+            errorPlacement: function(error, element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight: function(element, errorClass, validClass) {
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                $(element).removeClass('is-invalid');
+            }
+        });
+
+        jQuery.validator.setDefaults({
+            errorElement: "span",
+            ignore: ":hidden:not(select.chosen-select)",
+            errorPlacement: function(error, element) {
+                // Add the `help-block` class to the error element
+                error.addClass("help-block");
+                if (element.prop("type") === "checkbox") {
+                    //                error.insertAfter(element.parent("label"));
+                    error.appendTo(element.parents("validate-parent"));
+                } else if (element.is("select.chosen-select")) {
+                    error.insertAfter(element.siblings(".chosen-container"));
+                } else if (element.prop("type") === "radio") {
+                    error.appendTo(element.parents("div.validate-parent"));
+                } else {
+                    error.insertAfter(element);
+                }
+            },
+            highlight: function(element, errorClass, validClass) {
+                jQuery(element).parents(".validate-parent").addClass("has-error").removeClass("has-success");
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                jQuery(element).parents(".validate-parent").removeClass("has-error");
+            }
+        });
+        jQuery.validator.addMethod("valide_code", function(value, element) {
+            return this.optional(element) || /^[a-zA-Z\s\d\_\-()]{1,100}$/.test(value);
+        }, "Please enter a valid Code");
+        jQuery.validator.addMethod("valid_name", function(value, element) {
+            return this.optional(element) || /^[a-zA-Z\s\.\&\-()]*$/.test(value);
+        }, "Please enter a valid name");
+        jQuery.validator.addMethod("valid_date", function(value, element) {
+            return this.optional(element) || /^\d{4}\-\d{2}\-\d{2}$/.test(value);
+        }, "Please enter a valid date ex. 2017-03-27");
+        jQuery.validator.addMethod("valide_email", function(value, element) {
+            return this.optional(element) || /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/.test(value);
+        }, "Please enter a valid email addresss");
+        jQuery.validator.addMethod("valid_lk_phone", function(value, element) {
+            return this.optional(element) || /^0[7][0-9]{8}$/.test(value);
+        }, "Please enter a valid phone number");
+        jQuery.validator.addMethod("valid_lk_nic", function(value, element) {
+            return this.optional(element) || /^([0-9]{9}[x|X|v|V]|[0-9]{12})$/.test(value);
+        }, "Please enter a valid NIC number");
 </script>
 @endsection
