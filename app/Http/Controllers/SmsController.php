203cef0b65f28e82d6fde6ed7b5795fc61177069
kasun <kasun@ceytechsystemsolutions.com>
2022-04-24 21:05:21 +0530

    sms layout changes updated on warning report

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -12,13 +12,20 @@ class SmsController extends Controller
     {
         $client_details = Client::find($request->client_id);
         $tel_no = preg_replace('/^\+?1|\|1|\D/', '', ($client_details->contact_no));
-        $sms_message = 'Obage ' . $client_details->industry_name . ' anka darana parisara arakshana balapathraya' . $request->expire_date .
-        ' dina kal ikuth weemata niyamithawa etha. Ebawin balapathraya aluth kirima sadaha "parisarika arakshana balapathraya 
-        warshikawa aluth kirima sadaha wu illumpathraya" idiripath karana men kaarunikawa danwami. Danatamath oba wisin ayadumpathak 
-        idiripath kara athnam meya nosalakaa harina men danwaa sitimi.
-        Wayamba Palath Parisara Adikariya (Provicial Environment Authority-NWP)'."\n".
-        '037-2225236'."\n".
-        '(This is a system generated message)';
+        $sms_message = 'Obage ' . $client_details->industry_name . ' anka darana parisara arakshana balapathraya '."\n"
+        .  $request->expire_date .' wana dina kal ikuth weemata niyamithawa etha.'."\n"
+        . 'Ebawin balapathraya aluth kirima sadaha "parisarika '."\n"
+        . 'arakshana balapathraya warshikawa aluth kirima ' ."\n"
+        . 'sadaha wu illumpathraya"'."\n"
+        .'idiripath karana men '."\n"
+        .'kaarunikawa danwami.'."\n"
+        .'Danatamath oba wisin'."\n"
+        .'ayadumpathak idiripath kara'."\n"
+        .'athnam meya nosalakaa harina men danwaa sitimi.'."\n"
+        .'Wayamba Palath Parisara Adikariya'."\n"
+        .'(Provicial Environment Authority-NWP)'."\n"
+        .'037-2225236'."\n"
+        .'(This is a system generated message)';
 
         $data = array(
             'SmsMessage' => $sms_message,
