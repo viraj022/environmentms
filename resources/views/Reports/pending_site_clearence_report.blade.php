a79268e08f43c46cdd958c1ff0f21789045ca9fb
kasun <kasun@ceytechsystemsolutions.com>
2022-04-29 15:56:09 +0530

    completed the pending site clearance and epl report

--- resources/views/Reports/pending_site_clearence_report.blade.php
+++ resources/views/Reports/pending_site_clearence_report.blade.php
@@ -38,7 +38,13 @@
                                   @foreach ($pending_site_clear_data as $data)
                                      <tr>
                                          <td>{{ ++$loop->index }}</td>
-                                         <td><a href="{{ (!isset($data->siteClearenceSession->client)) ? '-' : '/industry_profile/id/{{ $data->siteClearenceSession->client->id '}}" class="btn btn-dark" target="_blank">{{ (!isset($data->siteClearenceSession->client)) ? '-' : $data->siteClearenceSession->client->file_no }}</a></td>
+                                         @if (!isset($data->siteClearenceSession->client))
+                                           @continue
+                                         @endif
+                                         <td><a href="/industry_profile/id/{{ $data->siteClearenceSession->client->id }}"
+                                             class="btn btn-dark"
+                                             target="_blank">{{ !isset($data->siteClearenceSession->client->file_no) ? 'N/A' : $data->siteClearenceSession->client->file_no }}</a></td>
+
                                          <td>{{ (!isset($data->siteClearenceSession->client)) ? '-' : $data->siteClearenceSession->client->industry_name}}</td>
                                          <td>{{ (!isset($data->siteClearenceSession->client->pradesheeyasaba)) ? '-' : $data->siteClearenceSession->client->pradesheeyasaba->name }}</td>
                                          <td>{{ (!isset($data->siteClearenceSession->client->environmentOfficer->user)) ? 'Not Assigned' : $data->siteClearenceSession->client->environmentOfficer->user->first_name.' '.$data->siteClearenceSession->client->environmentOfficer->user->last_name}}</td>
