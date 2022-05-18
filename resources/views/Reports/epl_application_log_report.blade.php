f0f5739328319878e2166c1631889f6720bcf8be
kasun <kasun@ceytechsystemsolutions.com>
2022-05-18 12:47:14 +0530

    report file number links are configured and site clearence report cloumn issue has fixed

--- resources/views/Reports/epl_application_log_report.blade.php
+++ resources/views/Reports/epl_application_log_report.blade.php
@@ -21,7 +21,7 @@
             <tr>
                 <th>#</th>
                 <th>Date</th>
-                <th>EPL No</th>
+                <th>File No</th>
                 <th>Applications Name and Address</th>
                 <th>Industry</th>
                 <th>Location</th>
