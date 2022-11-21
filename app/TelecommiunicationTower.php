3f5f1e3ff042c033bc65ede3b9ea85618d9e0e0c
Imalka wijerathna <imalka@ceytech.lk>
2022-11-21 17:20:41 +0530

    model and migration for telecommiunication towers

---
+++ app/TelecommiunicationTower.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TelecommiunicationTower extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'contact_name',
+        'contact_address',
+        'contact_number',
+        'extent_of_land',
+        'land_owner_name',
+        'land_owner_address',
+        'land_owner_phone',
+        'pradeshiya_sabha',
+        'sketch_of_road',
+        'investment_amount',
+        'tower_height',
+        'tower_length',
+        'tower_width',
+        'power_requirements',
+        'des_minimize_lightening',
+        'lightening_monitoring',
+        'public_places_distance',
+        'catastrophic_event',
+        'towers_within_500m',
+        'nearest_residence_distance',
+        'houses_within_50m',
+    ];
+}
