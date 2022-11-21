863da5bf00085a9dd0314e8ceead1833037b2470
Imalka Wijerathna <piumiwijerathna1997@gmail.com>
2022-11-21 20:46:24 +0530

    telecommunication migration, model edits

--- app/TelecommunicationTower.php
+++ app/TelecommunicationTower.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class TelecommiunicationTower extends Model
+class TelecommunicationTower extends Model
 {
     use HasFactory, SoftDeletes;
 
--- database/migrations/2022_11_21_204357_create_telecommunication_towers_table.php
+++ database/migrations/2022_11_21_204357_create_telecommunication_towers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTelecommiunicationTowersTable extends Migration
+class CreateTelecommunicationTowersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTelecommiunicationTowersTable extends Migration
      */
     public function up()
     {
-        Schema::create('telecommiunication_towers', function (Blueprint $table) {
+        Schema::create('telecommunication_towers', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->text('address');
@@ -50,6 +50,6 @@ class CreateTelecommiunicationTowersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('telecommiunication_towers');
+        Schema::dropIfExists('telecommunication_towers');
     }
 }
