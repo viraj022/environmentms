c36edaa30667064ee4ffa5a8b8a258caed262d9f
Imalka wijerathna <imalka@ceytech.lk>
2022-12-29 16:09:06 +0530

    warning letter msg content changed

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -5,47 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Client;
+use App\EPL;
 
 class SmsController extends Controller
 {
     public function sendSms(Request $request)
     {
         $client_details = Client::find($request->client_id);
+        $epl = EPL::where('client_id', $client_details->id)->orderBy('created_at', 'desc')->first();
         $tel_no = preg_replace('/^\+?1|\|1|\D/', '', ($client_details->contact_no));
-        $sms_message = 'Obage ' . $client_details->industry_name . ' anka darana parisara arakshana balapathraya '."\n"
-        .  $request->expire_date .' wana dina kal ikuth weemata niyamithawa etha. Ebawin balapathraya aluth kirima sadaha "parisarika arakshana balapathraya warshikawa aluth kirima sadaha wu illumpathraya" idiripath karana men kaarunikawa danwami. Danatamath oba wisin ayadumpathak idiripath kara athnam meya nosalakaa harina men danwaa sitimi.'."\n"
-        .'Wayamba Palath Parisara Adikariya'."\n"
-        .'(Provicial Environment Authority-NWP)'."\n"
-        .'037-2225236'."\n"
-        .'(This is a system generated message)';
 
-       if(isset($request->PhoneNumber)){
-        $data = array(
-            'SmsMessage' => $sms_message,
-            'PhoneNumber' => $tel_no,
-            'CompanyId' => 'CEYTECHAPI394',
-            'Pword' => 'aQyp7glqK0',
-        );
+        $sms_message = 'ඔබගේ ' . $epl->code . ' අංක දරණ පරිසර ආරක්ෂණ බලපත්‍රය 20 ' . $request->expire_date . ' වන දින කල් ඉකුත් විමට නියමිතව ඇත.' . "\n" . 'එය අළුත් කිරිමට අයදුම්පත්‍රයක් ඉදිරිපත් කරන මෙන් කාරුණිකව ඉල්ලා සිටිමි.' . "\n" . 'දැනටමත් එය සිදුකර ඇත්නම් මෙය නොසලකන ලෙසද දැනුම් දෙමි.' . "\n"
+            . 'වයඹ පළාත් පරිසර අධිකාරිය' . "\n" . '(Provincial  Environmental Authority-NWP)' . "\n" . '037-2225236' . "\n" . '(This is a system generated message)';
 
-        $json_data = json_encode($data);
-        $ch = curl_init('http://smsm.lankabell.com:4040/Sms.svc/PostSendSms');
-        // Attach encoded JSON string to the POST fields
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+        if (isset($request->PhoneNumber)) {
+            $data = array(
+                'SmsMessage' => $sms_message,
+                'PhoneNumber' => $tel_no,
+                'CompanyId' => 'CEYTECHAPI394',
+                'Pword' => 'aQyp7glqK0',
+            );
 
-        // Set the content type to application/json
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+            $json_data = json_encode($data);
+            $ch = curl_init('http://smsm.lankabell.com:4040/Sms.svc/PostSendSms');
+            // Attach encoded JSON string to the POST fields
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
 
-        // Return response instead of outputting
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            // Set the content type to application/json
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 
-        // Execute the POST request
-        $result = curl_exec($ch);
+            // Return response instead of outputting
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        // Close cURL resource
-        curl_close($ch);
-        return array('status' => 1, 'mesg' => 'SMS successfully sent');
-       }else{
-        return array('status' => 2, 'mesg' => 'No telephone number found');
-       }
+            // Execute the POST request
+            $result = curl_exec($ch);
+
+            // Close cURL resource
+            curl_close($ch);
+            return array('status' => 1, 'mesg' => 'SMS successfully sent');
+        } else {
+            return array('status' => 2, 'mesg' => 'No telephone number found');
+        }
     }
 }
